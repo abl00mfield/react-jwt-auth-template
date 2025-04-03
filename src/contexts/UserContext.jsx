78fccc4f701b3b4698ae8f47c4ef76e1bce0797a
abl00mfield <amanda.bloomfield@gmail.com>
2025-04-03 12:04:44 -0600

    set up User provider in User context

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -3,8 +3,19 @@ import { createContext } from "react";
 const UserContext = createContext(); //this is a constructor
 
 const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null); // set the user to null at first
   //the children prop represents each component we provide context to
-  return <UserContext.Provider>{children}</UserContext.Provider>;
+  const value = { user, setUser }; //value as an object with the user and the ability to set the user
+  return (
+    <UserContext.Provider value={value}>
+      {/* The data we pass to the value prop above is now available to */}
+      {/* all the children of the UserProvider component. */}
+      {children}
+    </UserContext.Provider>
+  );
 };
 
-export { UserProvider };
+// When components need to use the value of the user context, they will need
+// access to the UserContext object to know which context to access.
+// Therefore, we export it here.
+export { UserProvider, UserContext };
